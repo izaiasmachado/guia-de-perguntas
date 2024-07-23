9bc4b9605433325d8397090f41ef41a61b8eae6b
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-07-23 10:52:37 -0300

    feat: hash new user password

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,9 +1,16 @@
 const prisma = require("../lib/prisma");
+const bcrypt = require("../lib/bcrypt");
 
 module.exports = {
   async create(userData) {
+    const hashedPassword = await bcrypt.hashPassword(userData.password);
+    const user = {
+      ...userData,
+      password: hashedPassword,
+    };
+
     const createdUser = await prisma.user.create({
-      data: userData,
+      data: user,
     });
     return this._serialize(createdUser);
   },
