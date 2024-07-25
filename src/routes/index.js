5a4d84f602b484daafe139486337f3eb9307c76f
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-07-25 16:14:36 -0300

    feat: 404 not found page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const AuthMiddleware = require("../middlewares/AuthMiddleware");
 const HomeController = require("../controllers/HomeController");
+const PageNotFoundController = require("../controllers/PageNotFoundController");
 
 const authRouter = require("./auth");
 const questionRouter = require("./question");
@@ -13,9 +14,6 @@ router.get("/", HomeController.index);
 router.use("/q", questionRouter);
 router.use("/auth", authRouter);
 
-router.use((req, res) => {
-  console.log("404");
-  return res.status(404).send("Not found");
-});
+router.use(PageNotFoundController.index);
 
 module.exports = router;
