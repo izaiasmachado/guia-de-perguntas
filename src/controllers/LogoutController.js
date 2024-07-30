4909f88ba81fb825bc4a9624bef0724a5c95852f
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-07-30 15:26:16 -0300

    refactor: remove console log

--- src/controllers/LogoutController.js
+++ src/controllers/LogoutController.js
@@ -1,7 +1,6 @@
 module.exports = {
   index(req, res) {
     res.clearCookie("authorization");
-    console.log("User logged out successfully");
     return res.redirect("/auth/login?logout=true");
   },
 };
